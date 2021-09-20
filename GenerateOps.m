a143af1fe6b7d31580df5fcba7385107d6138b27
ignwilliam <65494942+ignwilliam@users.noreply.github.com>
2021-09-20 16:31:18 +0800

    Change function input to io_config

--- GenerateOps.m
+++ GenerateOps.m
@@ -1,41 +1,41 @@
-function ops = GenerateOps(nX,nA,nY,nB,nZ,nC,Q)
+function ops = GenerateOps(io_config, level)
 
-% generate set of operators for Alice, Bob, Charlie to build the moment
-% matrix for a given level of the hierarchy Q
+% generate set of operators for Alice, Bob, Eve to build the moment
+% matrix for a given level of the hierarchy
+% we assume that Alice's and Bob's operators are projective measurements
+% and Eve's operators are arbitrary operators
+
 
 % inputs:
-    % nX: number of inputs for Alice
-    % nA: number of outputs for Alice
-    % nY: number of inputs for Bob
-    % nB: number of outputs for Bob
-    % nZ: number of inputs for Charlie
-    % nC: number of outputs for Charlie
-    % Q: level of the hierarchy
+    % io_config: input-output configuration
+    %            (a cell array that specifies the number of outputs for each input for each party)
+    % level: relaxation level of the hierarchy
 % output:
     % ops: set of operators associated to the moment matrix
 
 % NOTES:
-% the building blocks are projective measurements
 % we use structures to represent the operators, the data type for each
-% field is string that represents the inputs and outputs of each party (from left to right)
-    % status: indicates whether the operator is zero operator, identity operator or product of projectors
+% field is an array that represents the inputs and outputs of each party (from left to right)
+    % status: indicates whether the operator is zero operator ('0'), identity operator ('I') or product of operators ('1')
     % as: the settings of Alice's operators
     % ao: the outputs of Alice's operators
     % bs: the settings of Bob's operators
     % bo: the outputs of Bob's operators   
-    % cs: the settings of Charlie's operators
-    % co: the outputs of Charlie's operators
+    % cs: the settings of Eve's operators
+    % co: the outputs of Eve's operators
+    % cdagger: boolean variable that indicates whether the operator is hermitian conjugated
 
 % example 1:
     % A00 is Alice's projector corresponding to input 0 and ouput 0
     % then we have:
     % A00.status = '1'
-    % A00.as = '0'
-    % A00.ao = '0'
-    % A00.bs = ''
-    % A00.bo = ''
-    % A00.cs = ''
-    % A00.co = ''
+    % A00.as = 0
+    % A00.ao = 0
+    % A00.bs = []
+    % A00.bo = []
+    % A00.cs = []
+    % A00.co = []
+    % A00.cdagger = []
     
 % example 2:
     % A00B10 is an operator which corresponds to product of Alice's and
@@ -43,12 +43,12 @@ function ops = GenerateOps(nX,nA,nY,nB,nZ,nC,Q)
     % while Bob's input is 1 and his output is 0,
     % then we have:
     % A00B10.status = '1'
-    % A00B10.as = '0'
-    % A00B10.ao = '0'
-    % A00B10.bs = '1'
-    % A00B10.bo = '0'
-    % A00B10.cs = ''
-    % A00B10.co = ''
+    % A00B10.as = 0
+    % A00B10.ao = 0
+    % A00B10.bs = 1
+    % A00B10.bo = 0
+    % A00B10.cs = []
+    % A00B10.co = []
 
 % example 3:
     % A00A10 is an operator which corresponds to product of two of Alice's
@@ -57,12 +57,16 @@ function ops = GenerateOps(nX,nA,nY,nB,nZ,nC,Q)
     % input is 1 and output is 1)
     % then we have:
     % A00A10.status = '1'
-    % A00A10.as = '01'
-    % A00A10.ao = '00'
-    % A00A10.bs = ''
-    % A00A10.bo = ''
-    % A00A10.cs = ''
-    % A00A10.co = ''
+    % A00A10.as = [0,1]
+    % A00A10.ao = [0,0]
+    % A00A10.bs = []
+    % A00A10.bo = []
+    % A00A10.cs = []
+    % A00A10.co = []
+
+% number of settings
+nX = length(io_config{1}); nY = length(io_config{2}); nZ = length(io_config{3});
+
 
 S = []; % initialise the set
 
@@ -72,23 +76,24 @@ S(1).as = []; % Alice's setting
 S(1).ao = []; % Alice's output
 S(1).bs = []; % Bob's setting
 S(1).bo = []; % Bob's output
-S(1).cs = []; % Charlie's setting
-S(1).co = []; % Charlie's output
-S(1).cdagger = []; % Charlie's dagger
+S(1).cs = []; % Eve's setting
+S(1).co = []; % Eve's output
+S(1).cdagger = []; % Eve's dagger
 
 P = []; % Q1 operators
 i = 1;
 
 %% Alice's operators
 for x = 1:nX
+    nA = io_config{1}(x);
     for a = 1:nA-1
         P(i).status = '1'; % status: '0' is zero, '1' is non-empty, 'I' is identity
         P(i).as = x-1; % Alice's setting
         P(i).ao = a-1; % Alice's output
         P(i).bs = []; % Bob's setting
         P(i).bo = []; % Bob's output
-        P(i).cs = []; % Charlie's setting
-        P(i).co = []; % Charlie's output
+        P(i).cs = []; % Eve's setting
+        P(i).co = []; % Eve's output
         P(i).cdagger = [];
         i = i+1;
     end
@@ -96,21 +101,23 @@ end
 
 %% Bob's operators
 for y = 1:nY
+    nB = io_config{2}(y);
     for b = 1:nB-1
         P(i).status = '1'; % status: '0' is zero, '1' is non-empty, 'I' is identity
         P(i).as = []; % Alice's setting
         P(i).ao = []; % Alice's output
         P(i).bs = y-1; % Bob's setting
         P(i).bo = b-1; % Bob's output
-        P(i).cs = []; % Charlie's setting
-        P(i).co = []; % Charlie's output
+        P(i).cs = []; % Eve's setting
+        P(i).co = []; % Eve's output
         P(i).cdagger = [];
         i = i+1;
     end
 end
 
-%% Charlie's operators
+%% Eve's operators
 for z = 1:nZ
+    nC = io_config{3}(z);
     for c = 1:nC
         for dagger = 0:1
             P(i).status = '1'; % status: '0' is zero, '1' is non-empty, 'I' is identity
@@ -118,8 +125,8 @@ for z = 1:nZ
             P(i).ao = []; % Alice's output
             P(i).bs = []; % Bob's setting
             P(i).bo = []; % Bob's output
-            P(i).cs = z-1; % Charlie's setting
-            P(i).co = c-1; % Charlie's output
+            P(i).cs = z-1; % Eve's setting
+            P(i).co = c-1; % Eve's output
             P(i).cdagger = dagger;
             i = i+1;
         end
@@ -129,7 +136,7 @@ end
 %% operators for higher moments
 S = [S,P];
 
-for l = 2:Q
+for l = 2:level
     L = length(S);
     for j = 1:length(P)
         for k = 1:L
